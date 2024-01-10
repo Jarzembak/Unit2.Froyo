5f0029e3c676c81412b08d6599a438931d6d977f
Jarzembak <lee.jarzembak@gmail.com>
2024-01-10 09:08:00 -0500

    object created and working properly

--- javascript.js
+++ javascript.js
@@ -1,5 +1,21 @@
 let userOrder = prompt(
-  "Please enter your desired flavors, separated by commas, with no spaces."
+  "Please enter your desired flavors, separated by commas:"
   )
 
-  
+let flavors = userOrder.split(","); //split each comma separated entry into an array of strings
+
+for (let i in flavors) {
+  flavors[i].trim
+}
+
+// const orderObj = {}
+
+// for (let i in flavors) {
+//   if (flavors[i] in orderObj) {
+//     orderObj[flavors[i]]++; // increment the flavor's value by 1
+//     } else {
+//       orderObj[flavors[i]] = 1; // add the flavor and initialize the count to 1
+//     }
+//   }
+
+//   console.table(orderObj);
